ff846225baaeb702a43832dd7d4278f6076a353c
Ssamaeljs <jandryadrianhg2003@gmail.com>
2024-05-21 12:33:30 -0500

    commit

--- clase2.c
+++ clase2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
     int myid, numprocs, dato;
     MPI_Status status;
@@ -10,14 +10,23 @@ int main(int argc, char const *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     if (myid == 0){
-        MPI_Send(&myid, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+        MPI_Send(&myid, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        printf("Dato enviado del procesos 0 al proceso 1");
+        MPI_Send(&myid, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+        printf("Dato enviado del procesos 0 al proceso 2");
+        MPI_Send(&myid, 1, MPI_INT, 3, 0, MPI_COMM_WORLD);
+        printf("Dato enviado del procesos 0 al proceso 3");
+    
     }else{
         MPI_Recv(&dato, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
         printf("Dato recibido del procesos 0 al proceso 1");
+        MPI_Recv(&dato, 1, MPI_INT, 2, 0, MPI_COMM_WORLD, &status);
+        printf("Dato recibido del procesos 0 al proceso 2");
+        MPI_Recv(&dato, 1, MPI_INT, 3, 0, MPI_COMM_WORLD, &status);
+        printf("Dato recibido del procesos 0 al proceso 3");
+        
     }
-
     MPI_Finalize();
-
-
     return 0;
 }
