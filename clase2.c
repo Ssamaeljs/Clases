2d499c51622d0ffb030e7e6290a47000ecb5385e
Ssamaeljs <jandryadrianhg2003@gmail.com>
2024-05-21 12:55:08 -0500

    commit

--- clase2.c
+++ clase2.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int main(int argc, char **argv)
-{
-    int myid, numprocs, dato;
-    MPI_Status status;
-    MPI_Init(&argc, &argv);
+int main(int argc, char **argv) {
+    int myid, numprocs;
 
+
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-    switch (myid)
-    {
-    case 0:
-     //Enviar proceso 0 al proceso 1
-        MPI_Send(&myid, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        printf("Dato enviado del proceso 0 al proceso 1\n");
-        break;
-    case 2:
-        MPI_Send(&myid, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
-        printf("Dato enviado del proceso 0 al proceso 2\n");
-        break;
-    default:
-        MPI_Recv(&dato, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        printf("Dato recibido en el proceso %d\n", myid);
-        break;
+    printf("Número total de procesos: %d\n", numprocs);
+
+    if (myid < numprocs - 1) {
+        int data = 20;
+        int dest = myid + 1;
+        MPI_Send(&data, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        printf("Proceso %d ha enviado datos al proceso %d.\n", myid, dest);
+    }
+
+    if (myid > 0) {
+        int received_data;
+        int source = myid - 1;
+        MPI_Recv(&received_data, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Proceso %d ha recibido datos del proceso %d. Los datos recibidos son: %d\n", myid, source, received_data);
     }
 
     MPI_Finalize();
